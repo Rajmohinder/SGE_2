e53c9c2c84a5d807c1b736dea07af9b77321d90e
Rajmohinder kaur <Rajmohinder kaur@DESKTOP-R5PRAV4.cogeco.local>
2024-02-18 19:12:04 -0500

    SGE_2 uno card game

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -8,7 +8,7 @@ package midtermreviewcodeforpartc;
 /**
  * A class that models a User for Uno Online.
  * The user has a name and a password.
- * @author dancye, 2019
+ * @author Rajmohinder Kaur, 2024
  */
 public class User 
 {
